b23f172a226130aae2a45f57ab2c4a64597dc27a
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-07-05 13:36:59 +0000

    Wrote more tests for the CharacterAbilityScores component

--- client/__tests__/components/CreateCharacterPage/CharacterClass.test.tsx
+++ client/__tests__/components/CreateCharacterPage/CharacterClass.test.tsx
@@ -1,11 +1,24 @@
 import { describe, it } from "vitest";
 import CharacterClass from "../../../src/components/CreateCharacterPage/CharacterClass";
-import { render, screen } from "@testing-library/react";
+import { render, screen, type RenderResult } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 
 describe("CharacterAbilityScores component", () => {
+    var classComponent: RenderResult;
+    beforeEach(() => {
+        classComponent = render(<CharacterClass />);
+    });
+    
     it("renders", () => {
-        render(<CharacterClass />);
-
         expect(screen.getByText(/class/i)).toBeInTheDocument();
     });
-})
+
+    it("allows the user to select from a list of classes, displaying more information on select", async () => {
+        const classComboBox = screen.getByRole("combobox", { name: /choose a class/i});
+        await userEvent.selectOptions(classComboBox, "barbarian");
+
+        expect(screen.getByText(/barbarian/i)).toBeInTheDocument();
+        expect(screen.getByText(/a tall human tribesman strides through a blizzard/i)).toBeInTheDocument();
+    });
+});
