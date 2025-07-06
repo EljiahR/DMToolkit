a638305b4b4761874f67a6235f299a5ec087d60b
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-07-06 16:47:40 +0000

    Added base tests for the ability score components

--- client/__tests__/components/CreateCharacterPage/CharacterAbilityScores.test.tsx
+++ client/__tests__/components/CreateCharacterPage/CharacterAbilityScores.test.tsx
@@ -1,11 +1,24 @@
 import { describe, it } from "vitest";
 import CharacterAbilityScores from "../../../src/components/CreateCharacterPage/CharacterAbilityScores";
-import { render, screen } from "@testing-library/react";
+import { render, screen, type RenderResult } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("CharacterAbilityScores component", () => {
-    it("renders", () => {
-        render(<CharacterAbilityScores />);
-
+    var abilityComponent: RenderResult;
+    beforeEach(() => {
+        abilityComponent = render(<CharacterAbilityScores />);
+    })
+    
+    it("renders with the standard array as default", () => {
         expect(screen.getByText(/ability scores/i)).toBeInTheDocument();
+        expect(screen.getByRole("heading", { name: /standard array/i })).toBeInTheDocument();
+    });
+
+    it("allows the user to change which method they use to get their scores", async () => {
+        const methodBox = screen.getByLabelText(/select the method/i);
+
+        await userEvent.selectOptions(methodBox, "random");
+
+        expect(screen.getByRole("heading", { name: /random roll/i })).toBeInTheDocument();
     });
 })
