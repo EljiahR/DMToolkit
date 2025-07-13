19f0ba4295a734ba759d2e2f9723e560c9de0293
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-07-13 13:12:19 +0000

    CharacterClass component should be caught up

--- client/src/components/CreateCharacterPage/CharacterClass.tsx
+++ client/src/components/CreateCharacterPage/CharacterClass.tsx
@@ -4,21 +4,36 @@ import { setCharacterClassBase } from "../../lib/redux/newCharacterSlice";
 
 
 export default function() {
+    const availableClasses = useAppSelector((state) => state.dmTools.characterClass);
     const selectedClass = useAppSelector((state) => state.newCharacter.characterClassBase);
     const dispatch = useAppDispatch();
     
     useLayoutEffect(() => {
+        if (availableClasses.length < 1) {
+            // throw error or return to homepage not sure yet :)
+        }
+        
         if (selectedClass == null) {
-            dispatch(setCharacterClassBase())
+            dispatch(setCharacterClassBase(availableClasses[0]));
         }
     });
+
+    const handleClassSelection = (classId: string) => {
+        const newClass = availableClasses.find((characterClass) => characterClass.id === classId);
+
+        if (newClass == undefined) {
+            // MORE ERROR HANDLING
+        }
+
+        dispatch(setCharacterClassBase(newClass!));
+    }
 
     return (
         <div>
             <h2>Class</h2>
             <label htmlFor="class-selector">Select a class</label>
-            <select id="class-selector" value={selectedClass.id} onChange={(e) => setSelectedClassId(e.target.value)}>
-                {classes.map((characterClass) => {
+            <select id="class-selector" value={selectedClass!.id} onChange={(e) => handleClassSelection(e.target.value)}>
+                {availableClasses.map((characterClass) => {
                     return (
                         <option key={`class-${characterClass.id}`} value={characterClass.id}>{characterClass.name}</option>
                     )
