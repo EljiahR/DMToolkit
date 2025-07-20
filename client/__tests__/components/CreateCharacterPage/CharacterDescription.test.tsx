960ea8d2f932857d76362e6d91f621a755c8b773
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-07-20 15:33:06 +0000

    Small additions to CharacterDescription component tests

--- client/__tests__/components/CreateCharacterPage/CharacterDescription.test.tsx
+++ client/__tests__/components/CreateCharacterPage/CharacterDescription.test.tsx
@@ -20,4 +20,24 @@ describe("CharacterAbilityScores component", () => {
 
         expect(nameInput.value).toBe("John DnD");
     });
+
+    it("allows the user to input their own physical and personality traits", async () => {
+        const physicalInput: HTMLInputElement = screen.getByLabelText(/physical traits/i);
+        const personalityInput: HTMLInputElement = screen.getByLabelText(/personality traits/i);
+        
+        await userEvent.type(physicalInput, "long, tan, and handsome");
+        await userEvent.type(personalityInput, "bit of an arse");
+
+        expect(physicalInput.value).toBe("long, tan, and handsome");
+        expect(personalityInput.value).toBe("bit of an arse");
+    });
+
+    it("allows the user to randomly generate physical and personality traits based on their scores", async () => {
+        const randomButtons: HTMLButtonElement[] = screen.getAllByLabelText(/randomize/i);
+
+        await userEvent.click(randomButtons[0]);
+        await userEvent.click(randomButtons[1]);
+
+        const firstPhysicalTrait = screen.getByLabelText(/physical traits/i)
+    });
 })
