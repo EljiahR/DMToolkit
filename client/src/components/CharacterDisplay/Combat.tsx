7c53259359226ef47b383fd1559adf043d222c33
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-08-03 12:47:27 +0000

    Built a bit of a skeleton for the Combat component

--- client/src/components/CharacterDisplay/Combat.tsx
+++ client/src/components/CharacterDisplay/Combat.tsx
@@ -1,5 +1,13 @@
 export default function() {
     return (
-        <div></div>
+        <div id="combat-tab">
+            <div id="health-ac">
+                <div id="ac"></div>
+                <div id="health"></div>
+            </div>
+            <div id="weapons-and-attacks"></div>
+            <div id="death-throws"></div>
+            <div id="reroll"></div>
+        </div>
     )
 }
