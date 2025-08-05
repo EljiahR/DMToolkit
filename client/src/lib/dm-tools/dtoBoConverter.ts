a4a8e4dedf322232353b89b13d66f8f624247d48
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-08-05 00:02:32 +0000

    Further additions to the dtoBoConverter functions

--- client/src/lib/dm-tools/dtoBoConverter.ts
+++ client/src/lib/dm-tools/dtoBoConverter.ts
@@ -1,11 +1,32 @@
-import type { FeatEffectDto, FeatureBaseDto } from "../types/apiResponses";
-import type { FeatureBase } from "../types/dmToolTypes";
+import type { BackgroundBaseDto, FeatEffectDto, FeatureBaseDto, SubclassBaseDto } from "../types/apiResponses";
+import type { BackgroundBase, FeatureBase, SubclassBase } from "../types/dmToolTypes";
 
-const featureBaseToBo = (featureDto: FeatureBaseDto, effects: FeatEffectDto[]): FeatureBase => {
+// Bases
+
+export const featureBaseToBo = (featureDto: FeatureBaseDto, effects: FeatEffectDto[]): FeatureBase => {
     return {
         id: featureDto.id,
         name: featureDto.name,
         description: featureDto.description,
-        availableEffects: 
+        availableEffects: featureDto.availableEffectIds.map((idArr) => idArr.map(id => effects.find((effect) => effect.id == id)).filter(effect => effect != undefined))
     }
 }
+
+export const backgroundBaseToBo = (backgroundDto: BackgroundBaseDto, featuresBases: FeatureBase[]): BackgroundBase => {
+    return {
+        id: backgroundDto.id,
+        name: backgroundDto.name,
+        description: backgroundDto.description,
+        abilityScores: backgroundDto.abilityScores,
+        features: featuresBases.filter((featureBase) => backgroundDto.featureIds.includes(featureBase.id)),
+        skillProficiencies: backgroundDto.skillProficiencies
+    }
+}
+
+export const subclassBaseToBo = (subclassDto: SubclassBaseDto): SubclassBase => {
+    return {
+        id: subclassDto.id,
+        name: subclassDto.name
+    }
+}
+
