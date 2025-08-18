15f0d698c2bddf030877115ef06e77a23f07d18c
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-08-18 13:24:36 +0000

    Added a few item interfaces

--- web-client/src/lib/types/dm-tool-types/items.ts
+++ web-client/src/lib/types/dm-tool-types/items.ts
@@ -3,3 +3,49 @@ export interface Piece {
     shortName: string;
     nextStep: number;
 }
+
+export interface Worth {
+    cp: number;
+    sp: number;
+    ep: number;
+    gp: number;
+    pp: number;
+}
+
+export interface Item {
+    id: string;
+    name: string;
+    weight: number;
+    worth: Worth;
+}
+
+export interface WeaponProperty {
+    id: string;
+    name: string;
+    description: string;
+}
+
+export interface WeaponMastery {
+    id: string;
+    name: string;
+    description: string;
+}
+
+export interface Weapon extends Item {
+    dice: [string, string];
+    damageType: string;
+    properties: WeaponProperty[];
+    mastery: WeaponMastery;
+    customName: string;
+}
+
+export interface Armor extends Item {
+    category: "Light" | "Medium" | "Heavy" | "Shield";
+    acBase: number;
+    dexterityCap: number;
+    hasDexterityCap: number;
+    strengthRequirement: number;
+    hasStealthDisadvantage: boolean;
+    don: string;
+    doff: string;
+}
