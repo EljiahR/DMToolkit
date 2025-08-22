17ee40916d8faf1228c1a69e35cf68560e0ef346
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-08-22 23:54:39 +0000

    Plugged in everything to the Character Display page

--- web-client/src/pages/CharacterDisplay.tsx
+++ web-client/src/pages/CharacterDisplay.tsx
@@ -1,5 +1,31 @@
+import Combat from "../components/CharacterDisplay/Combat";
+import Features from "../components/CharacterDisplay/Features";
+import Inventory from "../components/CharacterDisplay/Inventory";
+import Navigation from "../components/CharacterDisplay/Navigation";
+import Overview from "../components/CharacterDisplay/Overview";
+import Spells from "../components/CharacterDisplay/Spells";
+import Stats from "../components/CharacterDisplay/Stats";
+import { useAppSelector } from "../lib/redux/hooks"
+import { selectCharacterDisplay } from "../lib/redux/uiSlice"
+
+const view = {
+    "Overview": <Overview />,
+    "Stats": <Stats />,
+    "Combat": <Combat />,
+    "Feats": <Features />,
+    "Spells": <Spells />,
+    "Inventory": <Inventory />
+}
+
 export default function() {
+    const displayedPage = useAppSelector(selectCharacterDisplay);
+    
     return (
-        <div></div>
+        <div id="character-display">
+            <div id="view-container">
+                {view[displayedPage]}
+            </div>
+            <Navigation />
+        </div>
     )
 }
