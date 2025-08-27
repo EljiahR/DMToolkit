0fd215fbd772a9d843958184e3d1289189c956b6
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-08-27 14:48:39 +0000

    Finished and passed test for the creating a new character starting from the homepage

--- web-client/__tests__/userFlow/CreatingNewCharacter.test.tsx
+++ web-client/__tests__/userFlow/CreatingNewCharacter.test.tsx
@@ -3,13 +3,16 @@ import App from "../../src/App"
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import renderWithAll from "../renderOptions/renderWithAll";
+import { characterClasses } from "../seedData/characterClasses";
+import { characterBackgrounds } from "../seedData/characterBackgrounds";
+import { characterSpecies, lineages } from "../seedData/characterSpecies";
 
 
 vi.mock("./AuthProvider", () => ({}));
 
 describe("Creating a new character starting from the home page", () => {
     beforeEach(() => {
-        renderWithAll(<App />);
+        renderWithAll(<App />, { initialEntries: ["/"], preloadedState: {dmTools: { characterClasses, backgrounds: characterBackgrounds, species: characterSpecies, lineages: lineages } } });
     });
 
     it("allows the user to go to the Create Character section, make a new character following all steps, and view that new character on the display page", async () => {
@@ -17,11 +20,55 @@ describe("Creating a new character starting from the home page", () => {
         const createCharacterButton = screen.getByRole("button", {name: /create character/i});
         await userEvent.click(createCharacterButton);
 
-        // Arriving at the SignInPage and clicking the Continue without account button
+        // SignInPage and clicking the Continue without account button
         const anonymousButton = screen.getByRole("button", {name: /continue without account/i});
         await userEvent.click(anonymousButton);
 
-        // Arriving at the CreatePlayerCharacterPage
-        expect(screen.getByText(/create a new player character/i)).toBeInTheDocument();
+        // Start section
+        const startButton = screen.getByRole("button", { name: /start/i });
+        await userEvent.click(startButton);
+        
+        // Class section
+        const backgroundButton = screen.getByRole("button", { name: /background/i });
+        const classBox = screen.getByLabelText(/select a class/i);
+        await userEvent.selectOptions(classBox, "beefcake");
+        await userEvent.click(backgroundButton);
+
+        // Background section
+        const speciesButton = screen.getByRole("button", { name: /species/i });
+        const backgroundBox = screen.getByLabelText(/choose a background/i);    
+        await userEvent.selectOptions(backgroundBox, "ahcrap");
+        await userEvent.click(speciesButton);
+
+        // Species section
+        const scoreButton = screen.getByRole("button", { name: /ability scores/i });
+        const speciesBox = screen.getByLabelText(/select a species/i);
+        await userEvent.selectOptions(speciesBox, "hboi");
+        await userEvent.click(scoreButton);
+
+        // Ability Scores section
+        const alignmentButton = screen.getByRole("button", { name: /alignment/i });
+        const methodBox = screen.getByLabelText(/select the method/i);
+        await userEvent.selectOptions(methodBox, "manual");
+        const strInput: HTMLInputElement = screen.getByLabelText(/strength/i);
+        await userEvent.clear(strInput);
+        await userEvent.type(strInput, "12");
+        await userEvent.tab();
+        await userEvent.click(alignmentButton);
+
+        // Alignment section
+        const descriptionButton = screen.getByRole("button", { name: /description/i }); 
+        const lawfulGoodOption: HTMLInputElement = screen.getByLabelText(/lawful good/i);
+        await userEvent.click(lawfulGoodOption);
+        await userEvent.click(descriptionButton);
+
+        // Description section
+        const finishButton = screen.getByRole("button", { name: /finish/i});
+        const nameInput: HTMLInputElement = screen.getByLabelText(/name/i);
+        await userEvent.type(nameInput, "John DnD");
+        await userEvent.click(finishButton);
+
+        // Arriving at display page, overview section
+        expect(screen.getByRole("heading", { name: /john dnd/i })).toBeInTheDocument();
     })
 })
