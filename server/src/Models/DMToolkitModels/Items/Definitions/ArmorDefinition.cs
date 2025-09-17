f990f91313d1d2044be427deb93ef27e648823a3
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-09-17 20:50:33 +0000

    Finished the ArmorDefinition model

--- server/src/Models/DMToolkitModels/Items/Definitions/ArmorDefinition.cs
+++ server/src/Models/DMToolkitModels/Items/Definitions/ArmorDefinition.cs
@@ -6,4 +6,10 @@ namespace DMToolkit.Models.Items.Definitions;
 public class ArmorDefinition : ItemBase
 {
     public ArmorCategory ArmorCategory { get; set; } = ArmorCategory.Light;
+    public int DexterityCap { get; set; } = 0;
+    public bool HasDexterityCap { get; set; } = false;
+    public int StrengthRequirement { get; set; } = 0;
+    public bool HasStealthDisadvantage { get; set; } = false;
+    public string Don { get; set; } = string.Empty;
+    public string Doff { get; set; } = string.Empty;
 }
