55b77f7b6764a9a3c04d82c01d842e6e088235df
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-10-05 17:07:43 +0000

    Created the StartSetDto model to reflect all information a client will need to function

--- shared/Dtos/Collections/StarterSetDto.cs
+++ shared/Dtos/Collections/StarterSetDto.cs
@@ -1,6 +1,17 @@
+using DMToolkit.Shared.Models.Dtos.Definitions;
+using DMToolkit.Shared.Models.Dtos.Entities;
+
 namespace DMToolkit.Shared.Models.Dtos.Collections;
 
 public class StarterSetDto
 {
-    
+    public List<AbilityScoreDefinitionDto> AbilityScoreDefinitions { get; set; } = new();
+    public List<BackgroundDefinitionDto> BackgroundDefinitions { get; set; } = new();
+    public List<CharacterClassDefinitionDto> CharacterClassDefinitions { get; set; } = new();
+    public List<FeatDefinitionDto> Feats { get; set; } = new();
+    public List<SpeciesDefinitionDto> Species { get; set; } = new();
+    public List<FeatEffectDto> FeatEffects { get; set; } = new();
+    public List<SchoolDto> Schools { get; set; } = new();
+    public List<SpellDto> Spells { get; set; } = new();
+    public List<SpellEffectDto> SpellEffects { get; set; } = new();
 }
