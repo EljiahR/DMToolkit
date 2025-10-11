eec6ed259ed23922141495a72342a0942577d738
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-10-11 12:53:22 +0000

    Minor types

--- server/src/Models/DMToolkitModels/Collections/StartupData.cs
+++ server/src/Models/DMToolkitModels/Collections/StartupData.cs
@@ -8,8 +8,8 @@ public class StartupData
     public List<AbilityScoreDefinition> AbilityScoreDefinitions { get; set; } = new();
     public List<BackgroundDefinition> BackgroundDefinitions { get; set; } = new();
     public List<CharacterClassDefinition> CharacterClassDefinitions { get; set; } = new();
-    public List<FeatDefinition> Feats { get; set; } = new();
-    public List<SpeciesDefinition> Species { get; set; } = new();
+    public List<FeatDefinition> FeatDefinitions { get; set; } = new();
+    public List<SpeciesDefinition> SpeciesDefinitions { get; set; } = new();
     public List<FeatEffect> FeatEffects { get; set; } = new();
     public List<School> Schools { get; set; } = new();
     public List<Spell> Spells { get; set; } = new();
