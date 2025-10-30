274f5290121c4bf384f35bb39790d899386c48b2
EljiahR <kcerhajile@gmail.com>
2025-10-30 10:15:51 -0400

    /in server, fixed some minor errors

--- server/src/Data/Configurations/Definitions/SpeciesDefinitionConfiguration.cs
+++ server/src/Data/Configurations/Definitions/SpeciesDefinitionConfiguration.cs
@@ -1,5 +1,6 @@
 using DMToolkit.API.Models.DMToolkitModels.Definitions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DMToolkit.API.Data.Configurations.Definitions;
@@ -16,7 +17,12 @@ public class SpeciesDefinitionConfiguration : IEntityTypeConfiguration<SpeciesDe
         builder.Property(s => s.Sizes)
             .HasConversion(
                 v => string.Join(',', v),
-                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList(),
+                new ValueComparer<List<string>>(
+                    (c1, c2) => c1.SequenceEqual(c2),
+                    c => c.Aggregate(0, (hash, value) => HashCode.Combine(hash, value.GetHashCode())),
+                    c => c.ToList()
+                )
             );
     }
 }
--- server/src/Data/Configurations/JoinTables/CharacterCharacterClassInstanceConfiguration.cs
+++ server/src/Data/Configurations/JoinTables/CharacterCharacterClassInstanceConfiguration.cs
@@ -1,4 +1,5 @@
 using DMToolkit.API.Models.DMToolkitModels.JoinTables;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DMToolkit.API.Data.Configurations.JoinTables;
@@ -10,7 +11,12 @@ public class CharacterCharacterClassInstanceConfiguration : JoinTableKeyConfigur
         builder.Property(t => t.HpRolls)
             .HasConversion(
                 v => string.Join(',', v),
-                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList(),
+                new ValueComparer<List<int>>(
+                    (c1, c2) => c1.SequenceEqual(c2),
+                    c => c.Aggregate(0, (hash, value) => HashCode.Combine(hash, value.GetHashCode())),
+                    c => c.ToList()
+                )
             );
     }
 }
