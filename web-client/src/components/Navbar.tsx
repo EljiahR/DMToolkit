f21fdd62218136ca182132867a2c660f77fa043b
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-11-03 20:06:28 +0000

    In web client, i dont remember

--- web-client/src/components/Navbar.tsx
+++ web-client/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     const username = useAppSelector((state) => state.user.username);
     
     return (
-        <div className="w-full flex items-center justify-between px-2 py-1">
+        <div id="navbar" className="w-full flex items-center justify-between px-2 py-1">
             <NavigationLinks className="flex" />
             {username != null && username?.trim() != "" && 
             <div>Hello {username}</div>
