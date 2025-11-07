f1a2e844bcce5e16ede48b070fc57c530ef6258b
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-11-07 12:26:18 +0000

    In web cliemt, small updates to the character display overview

--- web-client/src/components/CharacterDisplay/Overview.tsx
+++ web-client/src/components/CharacterDisplay/Overview.tsx
@@ -4,29 +4,32 @@ const Overview = () => {
     const selectedCharacter = useAppSelector((state) => state.selectedCharacter);    
     
     return (
-        <div id="character-overview-page" className="flex flex-col">
-            <div id="basic-overview">
-                <h2>{selectedCharacter.name}</h2>
-                <p>{selectedCharacter.characterClassInstances[0].definition.name}</p>
+        <div id="character-overview-page" className="flex flex-col gap-3">
+            <div id="mechanical-description" className="card">
+                <div id="basic-overview">
+                    <h2>{selectedCharacter.name}</h2>
+                    <p>{selectedCharacter.characterClassInstances[0].definition.name}</p>
+                </div>
+                <div id="origin">
+                    <p>{selectedCharacter.speciesInstance?.definition.name ?? ""}</p>
+                    <p>{selectedCharacter.backgroundInstance?.definition.name ?? ""}</p>
+                </div>
+                <div id="alignment">
+                    <p>{selectedCharacter.alignment}</p>
+                </div>
             </div>
-            <div id="origin">
-                <p>{selectedCharacter.speciesInstance?.definition.name ?? ""}</p>
-                <p>{selectedCharacter.backgroundInstance?.definition.name ?? ""}</p>
-            </div>
-            <div id="alignment">
-                <p>{selectedCharacter.alignment}</p>
-            </div>
-            <div id="descriptions">
+           
+            <div id="rp-description" className="card flex flex-col gap-1">
                 <label htmlFor="physical-description">Physical</label>
-                <textarea id="physical-description" value={selectedCharacter.physicalDescription} readOnly />
+                <textarea className="text-input" id="physical-description" value={selectedCharacter.physicalDescription} readOnly />
                 <label htmlFor="personality-description">Personality</label>
-                <textarea id="personality-description" value={selectedCharacter.personality} readOnly />
+                <textarea className="text-input" id="personality-description" value={selectedCharacter.personality} readOnly />
                 <label htmlFor="ideals">Ideals</label>
-                <textarea id="ideals" value={selectedCharacter.ideals} readOnly />
+                <textarea className="text-input"id="ideals" value={selectedCharacter.ideals} readOnly />
                 <label htmlFor="bonds">Bonds</label>
-                <textarea id="bonds" value={selectedCharacter.bonds} readOnly />
+                <textarea className="text-input" id="bonds" value={selectedCharacter.bonds} readOnly />
                 <label htmlFor="flaws">Flaws</label>
-                <textarea id="flaws" value={selectedCharacter.flaws} readOnly />
+                <textarea className="text-input" id="flaws" value={selectedCharacter.flaws} readOnly />
             </div>
         </div>
         
