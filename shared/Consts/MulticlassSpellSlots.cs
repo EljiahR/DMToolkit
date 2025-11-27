b39eba4b268f1ffaa7256be4280b73fa8bad4c9a
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-11-27 03:18:49 +0000

    In shared, added a static class containing multiclass spell slot info

---
+++ shared/Consts/MulticlassSpellSlots.cs
@@ -0,0 +1,22 @@
+namespace SharedModels.Consts;
+
+public static class MulticlassSpellSlots
+{
+    public static readonly int[] LevelOneSlots =    [2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+    public static readonly int[] LevelTwoSlots =    [0, 0, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+
+    public static readonly int[] LevelThreeSlots =  [0, 0, 0, 0, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+
+    public static readonly int[] LevelFourSlots =   [0, 0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+
+    public static readonly int[] LevelFiveSlots =   [0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3];
+
+    public static readonly int[] LevelSixSlots =    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2];
+
+    public static readonly int[] LevelSevenSlots =  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2];
+
+    public static readonly int[] LevelEightSlots =  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1];
+
+    public static readonly int[] LevelNineSlots =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1];
+
+}
