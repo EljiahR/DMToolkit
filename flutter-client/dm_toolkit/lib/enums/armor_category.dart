fb107959e95d311060cb2b04199012432b8da8b2
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-12-10 20:40:24 +0000

    In flutter-client, added ArmorDefinition model.

---
+++ flutter-client/dm_toolkit/lib/enums/armor_category.dart
@@ -0,0 +1,6 @@
+enum ArmorCategory {
+  light,
+  medium,
+  heavy,
+  shield
+}
---
+++ flutter-client/dm_toolkit/lib/models/dm_toolkit/items/definitions/armor_definition.dart
@@ -0,0 +1,56 @@
+import 'package:dm_toolkit/enums/armor_category.dart';
+import 'package:dm_toolkit/models/dm_toolkit/items/bases/item_definition_base.dart';
+import 'package:dm_toolkit/models/dm_toolkit/items/bases/worth.dart';
+
+class ArmorDefinition extends ItemDefinitionBase {
+  ArmorCategory armorCategory;
+  int baseAC;
+  int dexterityCap;
+  bool hasDexterityCap;
+  int strengthRequirement;
+  bool hasStealthDisadvantage;
+  String don;
+  String doff;
+
+  ArmorDefinition({
+    required super.id,
+    required super.name,
+    required super.description,
+    required super.weight,
+    required super.itemType,
+    required super.worth,
+    required this.armorCategory,
+    required this.baseAC,
+    required this.dexterityCap,
+    required this.hasDexterityCap,
+    required this.strengthRequirement,
+    required this.hasStealthDisadvantage,
+    required this.don,
+    required this.doff
+  });
+
+  factory ArmorDefinition.fromJson(Map<String, dynamic> json) {
+    try {
+      var worth = Worth.fromJson(json['worth']);
+      
+      return ArmorDefinition(
+        id: json['id'] as String, 
+        name: json['name'] as String, 
+        description: json['description'] as String, 
+        weight: json['weight'] as int, 
+        itemType: json['itemType'] as String, 
+        worth: worth, 
+        armorCategory: json['armorCategory'] as ArmorCategory, 
+        baseAC: json['baseAC'] as int, 
+        dexterityCap: json['dexterityCap'] as int, 
+        hasDexterityCap: json['hasDexterityCap'] as bool, 
+        strengthRequirement: json['strengthRequirement'] as int, 
+        hasStealthDisadvantage: json['hasStealthDisadvantage'] as bool, 
+        don: json['don'] as String, 
+        doff: json['doff'] as String
+      );
+    } catch (e) {
+      throw FormatException('ArmorDefinition model invalid.', e);
+    }
+  }
+}
