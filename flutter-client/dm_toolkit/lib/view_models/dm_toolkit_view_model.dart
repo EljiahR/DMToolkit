c1cf324a594992bfd3d5bf2f5d7f14370dc229cb
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-12-14 15:01:35 +0000

    In flutter-client, fleshed out DMToolkitViewModel.init to make http GETs to a url specified in the .env filte

--- flutter-client/dm_toolkit/lib/view_models/dm_toolkit_view_model.dart
+++ flutter-client/dm_toolkit/lib/view_models/dm_toolkit_view_model.dart
@@ -1,18 +1,30 @@
+import 'dart:convert';
+
 import 'package:dm_toolkit/models/dm_toolkit/collections/startup_data.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class DMToolkitViewModel extends ChangeNotifier{
   final StartupData data = StartupData();
   String? errorMessage;
 
   Future<void> init() async {
-    try {
-      // Get json here
-      Map<String, dynamic> json = { "junk": "junk" };
-      data.importFromJson(json);
-    } catch (e) {
-      errorMessage = 'Could not get inital startup data.';
+    
+    final url = dotenv.env['SERVER_URL'];
+    if (url == null) {
+      errorMessage = '.env SERVER_URL was null.';
+      return;
+    } 
+    
+    final response = await http.get(Uri.parse(url));
+    
+    if (response.statusCode == 200) {
+      data.importFromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      errorMessage = 'Error: Data was unable to be retrieved';
     }
+
     notifyListeners();
   }
 }
