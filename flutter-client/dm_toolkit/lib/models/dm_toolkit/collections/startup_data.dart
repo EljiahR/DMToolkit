03bbc7e62353fe2b012aab73d9f9f1e8aa52ae79
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-12-18 01:57:29 +0000

    In flutter-client, copied the two load methods from the view model to the notifier with minor modifications

--- flutter-client/dm_toolkit/lib/models/dm_toolkit/collections/startup_data.dart
+++ flutter-client/dm_toolkit/lib/models/dm_toolkit/collections/startup_data.dart
@@ -137,4 +137,90 @@ class StartupData {
       throw FormatException('JSON for StartupData model is invalid.', e);
     }
   }
+
+  factory StartupData.fromJson(Map<String, dynamic> json) {
+    try {
+      var abilityScoreDefinitionListJson = json['abilityScoreDefinitions'] as List;
+      var abilityScoreDefinitions = abilityScoreDefinitionListJson
+        .map((abilityScoreDefinitionJson) => AbilityScoreDefinition.fromJson(abilityScoreDefinitionJson))
+        .toList();
+      
+      var skillDefinitions = abilityScoreDefinitions.fold(<SkillDefinition>[], (skillList, abilityScoreDefinition) => [...skillList, ...abilityScoreDefinition.skillDefinitions]);
+
+      var effectListJson = json['effects'] as List;
+      var effects = effectListJson
+        .map((effectJson) => Effect.fromJson(effectJson))
+        .toList();
+
+      var featDefinitionListJson = json['featDefinitions'] as List;
+      var featDefinitions = featDefinitionListJson
+        .map((featDefinitionJson) => FeatDefinition.fromJson(featDefinitionJson, effects))
+        .toList();
+
+      var itemDefinitionBaseListJson = json['itemDefinitionBases'] as List;
+      var itemDefinitionBases = itemDefinitionBaseListJson
+        .map((itemDefinitionBaseJson) {
+          switch (itemDefinitionBaseJson['itemType']) {
+            case 'Weapon':
+              return WeaponDefinition.fromJson(itemDefinitionBaseJson, effects);
+            case 'Armor':
+              return ArmorDefinition.fromJson(itemDefinitionBaseJson);
+            case 'Tool':
+              return ToolDefinition.fromJson(itemDefinitionBaseJson);
+            default:
+              return ItemDefinition.fromJson(itemDefinitionBaseJson);
+          }
+        })
+        .toList();
+
+      var backgroundDefinitionListJson = json['backgroundDefinitions'] as List;
+      var backgroundDefinitions = backgroundDefinitionListJson
+        .map((backgroundDefinitionJson) => BackgroundDefinition.fromJson(backgroundDefinitionJson, abilityScoreDefinitions, featDefinitions, skillDefinitions, itemDefinitionBases))
+        .toList();
+
+      var characterClassListJson = json['characterClassDefinitions'] as List;
+      var characterClassDefinitions = characterClassListJson
+        .map((characterClassJson) => CharacterClassDefinition.fromJson(characterClassJson, abilityScoreDefinitions, skillDefinitions, itemDefinitionBases, featDefinitions))
+        .toList();
+
+      var conditionListJson = json['conditionDefinitions'] as List;
+      var conditionDefinitions = conditionListJson
+        .map((conditionJson) => ConditionDefinition.fromJson(conditionJson, effects))
+        .toList();
+
+      var schoolJsonList = json['schools'] as List;
+      var schools = schoolJsonList
+        .map((schoolJson) => School.fromJson(schoolJson))
+        .toList();
+
+      var speciesListJson = json['speciesDefinitions'] as List;
+      var speciesDefinitions = speciesListJson
+        .map((speciesJson) => SpeciesDefinition.fromJson(speciesJson, featDefinitions))
+        .toList();
+
+      var spellListJson = json['spells'] as List;
+      var spells = spellListJson
+        .map((spellJson) => Spell.fromJson(spellJson, schools, characterClassDefinitions, itemDefinitionBases, effects))
+        .toList();
+      
+      return StartupData(
+        abilityScoreDefinitions: abilityScoreDefinitions,
+        backgroundDefinitions: backgroundDefinitions,
+        characterClassDefinitions: characterClassDefinitions,
+        conditionDefinitions: conditionDefinitions,
+        effects: effects,
+        featDefinitions: featDefinitions, 
+        itemDefinitionBases: itemDefinitionBases, 
+        schools: schools, 
+        speciesDefinitions: speciesDefinitions, 
+        spells: spells
+      );
+    } on FormatException catch (e) {
+      log('Error occured during StartupData json parse.', error: e);
+      rethrow;
+    }
+     catch (e) {
+      throw FormatException('JSON for StartupData model is invalid.', e);
+    }
+  }
 }
--- flutter-client/dm_toolkit/lib/notifiers/dm_toolkit_notifier.dart
+++ flutter-client/dm_toolkit/lib/notifiers/dm_toolkit_notifier.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:developer';
 import 'package:dm_toolkit/models/dm_toolkit/collections/startup_data.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'dm_toolkit_notifier.g.dart';
@@ -8,5 +14,37 @@ class DMToolkitNotifier extends _$DMToolkitNotifier {
   @override
   StartupData build() {
     return StartupData();
+  }
+
+  Future<void> fetchStartupData() async {
+    final url = dotenv.env['SERVER_URL'];
+    if (url == null) {
+      log('url was null.');
+      return;
+    } 
+    
+    log('Fetching data from $url');
+    final response = await http.get(Uri.parse('$url/DMToolkit'));
+    
+    if (response.statusCode == 200) {
+      state = StartupData.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      log('Data successfully retrieved');
+    } else {
+      log('Data unable to be retrieved');
+    }
+  }
+
+  Future<void> loadStartupDataFromSeedDataJson() async {
+    try {
+      final String response = await rootBundle.loadString("assets/dm_seed_data.json");
+      state = StartupData.fromJson(jsonDecode(response) as Map<String, dynamic>);
+      log('Data imported successfully.');
+    } on FlutterError catch (e) {
+      log('Error loading .json:', error: e);
+    } on FormatException catch (e) {
+      log('Error occured during json import.', error: e);
+    } catch (e) {
+      log('Unexpected error has occured:', error: e);
+    }
   }
 }
