920d90443b8d5f4e20c2360c74c2a0ef0664a56c
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2026-01-04 13:41:45 +0000

    In ./server/, updated DMUser model a bit for username instead of email usage and added a partially functional register endpoint for users

--- server/src/Controllers/DMUserController.cs
+++ server/src/Controllers/DMUserController.cs
@@ -48,4 +48,36 @@ public class DMUserController : ControllerBase
 
         return Ok(new { user = userDto });
     }
+
+    [HttpPost("Register")]
+    [AllowAnonymous]
+    public async Task<IActionResult> Register([FromBody] SignInDto model)
+    {
+        var existingUser = await _userManager.FindByNameAsync(model.Username);
+
+        if (existingUser != null)
+        {
+            return Conflict("Username already taken.");
+        }
+
+        var user = new DMUser
+        {
+            UserName = model.Username
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        var userDto = await _dmUserService.GetDMUserDtoAsync(user.Id);
+        if (userDto == null)
+        {
+            return NotFound("User was not found in database.");
+        }
+
+        return Ok(new { user = userDto });
+    }
 }
